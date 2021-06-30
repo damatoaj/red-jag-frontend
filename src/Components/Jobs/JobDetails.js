03188389d7a89729b327b9866c2c60f0d188fb73
damatoaj <mr.arthurdamato@gmail.com>
2021-06-30 15:49:47 -0700

    dummy data utilized in the jobs tabs

--- src/Components/Jobs/JobDetails.js
+++ src/Components/Jobs/JobDetails.js
@@ -2,16 +2,17 @@ import React from 'react'
 
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
 
 export default function JobDetails({data}) {
-    console.log(data)
-
     return data.map((job, index) => (
         <Card className="jobCard">
             <Card.Body>
-                <Card.Title>Card Title {job.id}</Card.Title>
-                <Card.Text>Location {job.title}</Card.Text>
+                <Card.Title> {job.title}</Card.Title>
+                <Card.Text> {job.company_name}</Card.Text>
+                <Card.Text> {job.location}</Card.Text>
+                <Card.Text> {job.via}</Card.Text>
+                <Card.Text> {job.detected_extensions.posted_at}</Card.Text>
+                <Card.Text> {job.detected_extensions.schedule_type}</Card.Text>
                 <Button>Add to Saved</Button>
             </Card.Body>
         </Card>
