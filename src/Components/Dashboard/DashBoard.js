81cb9e943dfeb716be08c036ad89ff447cfb041d
damatoaj <mr.arthurdamato@gmail.com>
2021-07-02 16:41:35 -0700

    almost have the dashboard done

--- src/Components/Dashboard/DashBoard.js
+++ src/Components/Dashboard/DashBoard.js
@@ -1,11 +1,77 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
+import { 
+    BsFillEyeSlashFill, 
+    BsEyeSlash, 
+    BsPencil,
+    BsEnvelope
+ } from 'react-icons/bs';
 
 import {
-    Table
+    Table,
+    Button
 } from 'react-bootstrap';
 
 function DashBoard({ savedJobsArray }) {
-    console.log(savedJobsArray)
+    const [status, setStatus] = useState('');
+    const [contactInfo, setContactInfo] = useState([]);
+    const [comment, setComment] = useState('');
+    const [editJob, setEditJob] = useState({});
+
+    const statusChange = (e) => {
+        setStatus(e.target.value)
+    };
+
+    const contactInfoChange = (e) => {
+        setContactInfo(e.target.value);
+        console.log(contactInfo)
+    };
+
+    const commentChange = (e) => {
+        setComment(e.target.value);
+    };
+
+    const editJobChange = (e) => {
+        console.log(e.target.value, 'click');
+        setEditJob(savedJobsArray[e.target.value])
+        console.log(editJob)
+        editJob.status = status;
+        editJob.comment = comment;
+        console.log(editJob)
+    };
+
+    let jobsRow = savedJobsArray.map((savedJob, index) => (
+        <tr>
+            <th>{savedJob.title}</th>
+            <th>{savedJob.company_name}</th>
+            <th>{savedJob.detected_extensions.posted_at}</th>
+            <th>
+                
+                <BsEnvelope />
+            </th>
+            <th>
+                <select name="status" onChange={statusChange}> 
+                    <option value="none" disabled hidden>Default</option>
+                    <option value="done" >Done</option>
+                    <option value="follow_up">Follow Up</option>
+                    <option value="act_now">Act Now</option>
+                </select>
+            </th>
+            <th>
+                <input placeholder="Previously saved comment" onChange={commentChange}>
+                
+                </input>
+            </th>
+            <th>
+                <Button onClick={editJobChange} value={index}>
+                    <BsPencil value={index}/>
+                </Button>
+            </th>
+            <th>
+                <BsEyeSlash />
+            </th>
+        </tr>
+    ))
     return (
         <Table>
             <thead>
@@ -21,7 +87,7 @@ function DashBoard({ savedJobsArray }) {
                 </tr>
             </thead>
             <tbody>
-
+                {jobsRow}
             </tbody>
         </Table>
     )
