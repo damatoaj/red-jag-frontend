3e698498fbcbcb922c905e8e4e2d65324bfe1cf6
damatoaj <mr.arthurdamato@gmail.com>
2021-07-03 14:45:35 -0700

    fixed saved job icon

--- src/Components/Jobs/JobDetails.js
+++ src/Components/Jobs/JobDetails.js
@@ -2,14 +2,14 @@
 
 import React from 'react'
 import { FaStar } from 'react-icons/fa';
+import { IconContext } from 'react-icons/lib';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import { Col } from 'react-bootstrap';
 import { Row } from 'react-bootstrap';
 
 export default function JobDetails({ data, savedJobsArray }) {
-    const saveJobHandler = (e) => {
-        let index = (e.target.value);
+    const saveJobHandler = (index) => {
         console.log(index)
         let theJob = data.jobs[index];
         savedJobsArray.push(theJob)
@@ -25,29 +25,35 @@ export default function JobDetails({ data, savedJobsArray }) {
     // can't set button to "selected/saved" color state attribute
     return data.jobs.map((job, index) => (
         <Card className="jobCard">
-            <Card.Body>
+            <IconContext.Provider value={{className:'react-icons'}}>
+                <Card.Body>
 
-                <Row>
-                
-                    <Col><Card.Title>{job.title}</Card.Title></Col>
-                    {/* ****test onCLick functionality code ****  onClick={() => alert('Hi! Admin')} */}
-                    <Col xs={2}><Button id="star-btn" value={index} onClick={saveJobHandler}><span><FaStar size={20}/></span></Button></Col>
+                    <Row>
+                    
+                        <Col><Card.Title>{job.title}</Card.Title></Col>
+                        {/* ****test onCLick functionality code ****  onClick={() => alert('Hi! Admin')} */}
+                        <Col xs={2}>
+                            <Button id="star-btn" onClick={(e)=> saveJobHandler(index)}>
+                                <FaStar size={20} />
+                            </Button>
+                        </Col>
 
-                </Row>
-                <Row>
-                    <Col><Card.Text>{job.company_name}</Card.Text></Col>
-                    <Col xs={4}><Card.Text>{job.detected_extensions.posted_at}</Card.Text></Col>
-                </Row>
-                <Row>
-                    <Col><Card.Text>{job.location}</Card.Text></Col>
-                    <Col xs={4}><Card.Text>{job.detected_extensions.schedule_type}</Card.Text></Col>
-                </Row>
+                    </Row>
+                    <Row>
+                        <Col><Card.Text>{job.company_name}</Card.Text></Col>
+                        <Col xs={4}><Card.Text>{job.detected_extensions.posted_at}</Card.Text></Col>
+                    </Row>
+                    <Row>
+                        <Col><Card.Text>{job.location}</Card.Text></Col>
+                        <Col xs={4}><Card.Text>{job.detected_extensions.schedule_type}</Card.Text></Col>
+                    </Row>
+                    
+                    {/* <Card.Text>{job.via}</Card.Text> */}
+                    
+                    
                 
-                {/* <Card.Text>{job.via}</Card.Text> */}
-                
-                
-               
-            </Card.Body>
+                </Card.Body>
+            </IconContext.Provider>
         </Card>
     ))
 }
